332d34afa34d8545e651145eb6713182c945abc8
Bianca Soares <119140865+biancasuarz@users.noreply.github.com>
2025-06-17 11:07:28 -0300

    Update remove_duplicados.py

--- remove_duplicadospy.py
+++ remove_duplicadospy.py
@@ -1,10 +1,9 @@
 def remove_duplicados(arr):
     return list(set(arr))
 
-# Teste da função remove_duplicados
 lista = [1, 2, 2, 3, 4, 4, 5,'a', 'b', 'a']
 resultado = remove_duplicados(lista)
-print(resultado)  # Saída: [1, 2, 3, 4, 5] (a ordem pode variar devido ao uso de set)
+print(resultado)
 
 def remove_duplicados_ordem(arr):
     unicos = []
@@ -13,6 +12,5 @@ def remove_duplicados_ordem(arr):
             unicos.append(item)
     return unicos
 
-# Teste da função remove_duplicados_ordem
 resultado_ordem = remove_duplicados_ordem(lista)
 print(resultado_ordem)  # Saída: [1, 2, 3, 4, 5]
